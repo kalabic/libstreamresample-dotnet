45f60ba8cf5043899078deb196c4debd360bd64f
Silvije Kalabic <28826780+kalabic@users.noreply.github.com>
2025-08-07 15:28:41 +0000

    The final license update. Continuing development elsewhere.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # libstreamresample-dotnet
 Experiment with audio resampling using fork of [xycui/libresamplesharp](https://github.com/xycui/libresamplesharp).
+
+Development will continue in other repository: [AudioFormatLib](https://github.com/kalabic/audioformatlib).
+
+This is derivative work with long history, and its history is the main reason switch to LGPL v2.1 was made, more here: [Breaking down licensing question](https://github.com/kalabic/audioformatlib/tree/main/src/AudioFormatLib/Resampler#breaking-down-licensing-question)
--- src/LibStreamResampler/ReSampler.cs
+++ src/LibStreamResampler/ReSampler.cs
@@ -1,12 +1,50 @@
-﻿/*
-
-MIT License, see: https://github.com/kalabic/libstreamresample-dotnet/blob/master/LICENSE
-
-History:
-- Original: https://github.com/xycui/libresamplesharp/blob/master/LibResample.Sharp/ReSampler.cs
-- Forked: https://github.com/kalabic/libstreamresample-dotnet/blob/master/src/LibStreamResampler/ReSampler.cs
-
-*/
+﻿/******************************************************************************
+ *
+ * libresamplesharp
+ * Copyright (c) 2018 Xianyi Cui
+ *
+ * libresample4j
+ * Copyright (c) 2009 Laszlo Systems, Inc
+ *
+ * libresample 0.1.3
+ * Copyright (c) 2003 Dominic Mazzoni
+ *
+ * Resample 1.7
+ * Copyright (c) 1994-2002 Julius O. Smith III
+ * 
+ * 'libresamplesharp' is a C# port of David Nault's 'libresample4j',
+ * which is a Java port of Dominic Mazzoni's 'libresample 0.1.3',
+ * which is in turn based on Julius Smith's 'Resample 1.7' library.
+ *
+ * This product includes software derived from the work of
+ * Julius Smith, Dominic Mazzoni, David Nault, and others.
+ * 
+ * - https://ccrma.stanford.edu/~jos/resample/
+ * - https://ccrma.stanford.edu/~jos/resample/Free_Resampling_Software.html
+ * - https://github.com/minorninth/libresample
+ * - https://github.com/dnault/libresample4j
+ * - https://github.com/xycui/libresamplesharp
+ *
+ *
+ * This file is part of AudioFormatLib, which is free software:
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License (LGPL), version 2.1 only,
+ * as published by the Free Software Foundation.
+ *
+ * AudioFormatLib is intended to be used as a dynamically linked library.
+ * Applications using this library are not subject to the LGPL license,
+ * provided they comply with its terms (e.g., allowing replacement of the library).
+ *
+ * AudioFormatLib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 2.1 along with this file. If not, see:
+ * https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html 
+ *
+ *****************************************************************************/
 
 using System;
 using System.Linq;
